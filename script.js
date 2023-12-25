5b2643b1a7a4276aca88aa077a822c80e6b862f0
03tb <132428099+03tb@users.noreply.github.com>
2023-12-25 21:20:41 +1100

    Dynamic div and default grid added

--- script.js
+++ script.js
@@ -1,27 +1,69 @@
 const canvasContainer = document.querySelector('#canvas-container');
 
-const layer = document.querySelector('#layer');
-
-
-function createRow (gridSize) {
-    const newRow = document.createElement('div');
-    newRow.classList.add('row');
-
-    for (let i=0; i<gridSize; i++) {
-        const addRow = document.createElement('div');
-        addRow.classList = ('row-div')
-        addRow.setAttribute('style', 'display: flex; border: 1px solid black; width: 40px; height: 40px')
-        newRow.appendChild(addRow);
-    }
-    layer.appendChild(newRow);
-}
-
-createRow(16);
-
-function createLayer(gridSize) {
-    for (let z=0; z<gridSize; z++) {
-        createRow(16);
+function insertCells(numDivs) {
+    for (let i = 0; i < numDivs; i++) {
+        const insertCells = document.createElement('div');
+        insertCells.className = 'cells';
+        canvasContainer.appendChild(insertCells);
     }
 }
 
-createLayer(16);
+let inputValue;
+
+let userGridButton = document.querySelector("#submit");
+
+userGridButton.addEventListener("click", () => {
+    canvasContainer.textContent = "";
+    gridPressed();
+    numDivs();
+    cellCalculation();
+    setStyle(cellCalc);
+})
+
+function gridPressed() {
+    let userInput = document.getElementById("number");
+    console.log(userInput.value)
+    if (userInput.value >= 2 && userInput.value <= 50) {
+        console.log('value is in the right range');
+        inputValue = userInput.value;
+        return inputValue;
+    }  else {
+        alert('Please choose a grid size between 2 and 50');
+    }
+}
+
+let numDivs2;
+
+function numDivs() {
+    let numDivs = inputValue * inputValue;
+    console.log(numDivs);
+    insertCells(numDivs, cellCalc);
+    numDivs2 = numDivs;
+ }
+
+let cellCalc;
+ 
+function cellCalculation() {
+    cellCalc = inputValue / numDivs2;
+    cellCalc = cellCalc * 100;
+    console.log('cell calc is: ' +  cellCalc);
+    return cellCalc;
+}
+
+function setStyle(cellCalc) {
+    const cells = document.querySelectorAll('.cells')
+
+    cells.forEach(cell => {
+        cell.style.flex = `1 0 calc(${cellCalc}%)`;
+    });
+}
+
+function defaultGrid() {
+    insertCells(256);
+    const cells = document.querySelectorAll('.cells')
+    cells.forEach(cell => {
+        cell.style.flex = "1 0 calc(6.25%)";
+    });
+}
+
+defaultGrid();
